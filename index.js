7abea88200ccc3d960c29c7461af4fcfcf0c4f94
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-11-14 19:34:56 +0100

    installing missing modules

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post(
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
