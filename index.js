f053c54853b8530fdcdff6136826962b06e8ea0b
m0ntz <109100764+m0ntz@users.noreply.github.com>
2023-01-13 15:43:33 +0100

    added allowed origins CORS

--- index.js
+++ index.js
@@ -8,9 +8,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
-app.use(cors());
 
-let auth = require("./auth")(app);
+// allow all origins
+// app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:1234",
+  "http://localhost:4200",
+  "https://favflix-app.netlify.app/",
+  "https://myfavflixapi.herokuapp.com/",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+//Authentication
+require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
